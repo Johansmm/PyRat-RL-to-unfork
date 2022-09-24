5f9d015714889d0932356043b8042e31a254459c
jmejia <jmejia@brainchipinc.com>
2022-09-24 02:09:30 +0200

    docs(simulator): append docstring in functions

--- simulator.py
+++ simulator.py
@@ -6,7 +6,31 @@ from resources.imports.maze import generate_pieces_of_cheese
 
 
 class PyRat(object):
-    def __init__(self, width=21, height=15, round_limit=200, cheeses=40, symmetric=False,
+    """Pyrat simulator, which performs a synchronous game between two players,
+    eliminating the animations to obtain faster games.
+
+    Similar behavior can be obtained on pyrat.py, from the command line:
+
+    .. code-block:: console
+        python pyrat.py -d 0 -md 0 --test 1 --synchronous --nodrawing
+
+    width : int, optional
+        Width of the maze (-x, --width), by default 21
+    height : int, optional
+        Height of the maze (-y, --height),, by default 15
+    round_limit : int, optional
+        Max number of turns (-mt, --max_turns), by default 200
+    cheeses : int, optional
+        Number of pieces of cheese (-p, --pieces), by default 41
+    symmetric : bool, optional
+        Enforce symmetry of the maze (not --nonsymetric), by default False
+    start_random : bool, optional
+        Players start at random location in the maze (--start_random), by default True
+    opponent : object, optional
+        Oponent AIs (--python), by default manh
+    """
+
+    def __init__(self, width=21, height=15, round_limit=200, cheeses=41, symmetric=False,
                  start_random=True, opponent=manh):
         self.preprocess = False
         self.symmetric = symmetric
@@ -22,9 +46,14 @@ class PyRat(object):
         self.reset()
 
     def _update_state(self, action, enemy_action):
-        """
-        Input: actions and states
-        Ouput: new states and reward
+        """Players' action, which update the state of the game
+
+        Parameters
+        ----------
+        action : str
+            Player action
+        enemy_action : str
+            Enemy action
         """
         (xx, yy) = self.enemy
         enemy_action_x = 0
@@ -67,6 +96,16 @@ class PyRat(object):
         self._draw_state()
 
     def _draw_state(self):
+        """Draw list of cheese and players' position on a maze of size (height, width, 3), where:
+            - cheese are in first dimension,
+            - player is in the second one and
+            - opponent is in the last one.
+
+        Returns
+        -------
+        np.ndarray
+            Current state of the maze with players' and cheeses position.
+        """
         im_size = (self.height, self.width, 3)
         self.canvas = np.zeros(im_size)
 
@@ -82,6 +121,13 @@ class PyRat(object):
         return self.canvas
 
     def _get_reward(self):
+        """Update scores and return reward of player
+
+        Returns
+        -------
+        float
+            Reward get for last action, between {0, 0.5, 1.0}
+        """
         if self.round > self.round_limit:
             return -1  # Lost for max_round
 
@@ -91,6 +137,13 @@ class PyRat(object):
         return self.score - old_score
 
     def _is_over(self):
+        """Check if game is over
+
+        Returns
+        -------
+        bool
+            Game's status
+        """
         if self.score > self.cheeses / 2 or self.enemy_score > self.cheeses / 2 or (
             (self.score == self.enemy_score) and self.score >= self.cheeses / 2
         ) or self.round > self.round_limit or len(self.piecesOfCheese) == 0:
@@ -99,9 +152,35 @@ class PyRat(object):
             return False
 
     def observe(self):
+        """Return the current maze map
+
+        Returns
+        -------
+        np.ndarray
+            Maze map of size (1, height, width, 3)
+        """
         return np.expand_dims(self.canvas, axis=0)
 
     def act(self, action):
+        """Advance one step in the game, where the following steps are executed:
+        1. Get action of enemy.
+        2. Update scores and :attr:`piecesOfCheese` from player and enemy actions.
+        3. Observes the state of the game
+
+        Parameters
+        ----------
+        action : str
+            Player action, one of ['D', 'U', 'L', 'R']
+
+        Returns
+        -------
+        np.ndarray
+            Maze map of size (1, height, width, 3)
+        float
+            Player's reward for this action
+        bool
+            Game's status: is finished or not
+        """
         enemy_action = self.opponent.turn(None, self.width, self.height, self.enemy,
                                           self.player, self.enemy_score, self.score,
                                           self.piecesOfCheese, 3000)
@@ -111,6 +190,8 @@ class PyRat(object):
         return self.observe(), reward, game_over
 
     def reset(self):
+        """Reset scores, positions and create a new list of cheeses
+        """
         self.piecesOfCheese, self.player, self.enemy = generate_pieces_of_cheese(
             self.cheeses, self.width, self.height, self.symmetric,
             (0, 0), (self.width - 1, self.height - 1), self.start_random)
